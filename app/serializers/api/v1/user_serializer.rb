e7c075d308eb4868c1b52b1325220ea499a578d9
FerozK-dev <ferozkhan.deve@gmail.com>
2024-12-03 00:09:03 +0200

    Made changes for deployment

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,7 +1,11 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :about_me, :headline, :location, :contact, :github_url, :linked_url, :profile_picture
+  attributes :id, :first_name, :last_name, :about_me, :headline, :location, :contact, :github_url, :linked_url, :profile_picture, :title, :work_email, :resume
 
   def profile_picture
     object.profile_picture.presence && UrlHelpers.rails_blob_url(object.profile_picture)
+  end
+
+  def resume
+    object.resume.presence && UrlHelpers.rails_blob_url(object.resume)
   end
 end
